89660dfcb126020146c1827681026003db3da9fb
Juan Luis Oropeza <juanloropeza@gmail.com>
2020-04-27 19:47:25 -0300

    ajustes

--- js/mis_workflows.js
+++ js/mis_workflows.js
@@ -18,10 +18,12 @@ $(document).ready(function () {
   /* CAMBIAR ICONOS DE LA FILA INTERNA DESPLEGABLE */
   $( ".workflows .content_table .div_table_wfl .div_tbody .table_row .tddiv_elements" ).on( "click", "a.intern_clp_link", function(e) {
     if($(this).hasClass('collapsed')) {
+      $(this).parent().parent().addClass('filaAbierta');
       $(this).find(".icon-icon_arrow-down1")
             .removeClass("icon-icon_arrow-down1")
             .addClass("icon-icon_arrow-right");
     } else {
+      $(this).parent().parent().removeClass('filaAbierta');
       $(this).find(".icon-icon_arrow-right")
             .removeClass("icon-icon_arrow-right")
             .addClass("icon-icon_arrow-down1");
